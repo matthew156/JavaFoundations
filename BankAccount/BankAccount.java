ab03b2ab31beca95a7320284ba0d3ad1540a9cb7
matthew156 <matthewastro@gmail.com>
2021-09-30 12:31:12 -0400

    Bank Account Second Commit 🚀

--- BankAccount/BankAccount.java
+++ BankAccount/BankAccount.java
@@ -2,20 +2,24 @@ public class BankAccount {
     private double checkingBalance;
     private double savingsBalance;
     public static int numOfAccounts = 0;
-    private double totalAmount = checkingBalance + savingsBalance;
-
+    public static double totalAmount = 0;
+    private String user;
 
 //*************CONSTRUCTOR *********************** */
-    public BankAccount() {
+    public BankAccount(String user, double checkingBalance, double savingsBalance) {
+        this.user = user;
+        this.checkingBalance = checkingBalance;
+        this.savingsBalance = savingsBalance;
         numOfAccounts++;
+        totalAmount += checkingBalance + savingsBalance;
     }
 
 //*************GET AND SET *********************** */
     public double getCheckingAccount(){
-        return checkingBalance;
+        return this.checkingBalance;
     }
     public double getSavingsAccount(){
-        return savingsBalance;
+        return this.savingsBalance;
     }
     public double depositCheckingAccount(double amount){
         return this.checkingBalance += amount;
@@ -26,6 +30,9 @@ public class BankAccount {
 public double totalAmount(double amount){
     return totalAmount;
 }
+public String getUserName(){
+    return this.user;
+}
 
 //******************METHODS*************************/
     public double withdrawCheckingAccount(double amount){
@@ -33,6 +40,9 @@ public double totalAmount(double amount){
     }
     public double withdrawSavingsAccount(double amount){
      return this.savingsBalance -= amount;
+    }
+    public void displayAccount(){
+        System.out.println("Welcome " + getUserName() + "!\n" + "Your Checking balance is: " + getCheckingAccount() + "\n" + "Your Savings balance is: " + getSavingsAccount());
     }
 
 }
--- BankAccount/Test.java
+++ BankAccount/Test.java
@@ -1,5 +1,12 @@
 public class Test {
     public static void main(String[] args) {
-        BankAccount account1 = new BankAccount();
+        BankAccount account1 = new BankAccount("Matt", 250, 100);
+        account1.displayAccount();
+        account1.depositCheckingAccount(100);
+        account1.depositSavingsAccount(100);
+        account1.withdrawCheckingAccount(50);
+        account1.withdrawSavingsAccount(50);
+        System.out.println(BankAccount.numOfAccounts);
+        System.out.println(BankAccount.totalAmount);
     }
 }
